14c0b08afd1257e50bb8a5e70e91fa94197e28cd
Dorian Mayamba <dorian.mayamba@gmail.com>
2021-02-24 04:39:27 +0100

    The data can be displayed from the Api now

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -14,7 +14,7 @@ const Tab1: React.FC = () => {
 
       <IonContent fullscreen>
 
-        <IonCard id="welcome">
+        <IonCard id="welcome" color="light">
           <IonCardHeader><IonCardTitle>Welcome to the Covid App</IonCardTitle></IonCardHeader>
           <IonCardContent>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
